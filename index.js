a6ca7f5282763d229e2f9abf718ab7c0e70eef68
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 15:40:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function produceDrivingRange() {
-  
+function produceDrivingRange(r) {
+  return function(a, b) {
+    let len = parseInt(a.substring(0, 2)) - parseInt(b.substring(0, 2))
+    if (len > r) {
+      return `'${r - len} blocks out of range'`
+    } else {
+      return `'within range by ${len - r}'`
+    }
+  }
 }
