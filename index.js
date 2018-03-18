7a453b05c74c392f9d0a436180f632b548f20ed7
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 15:41:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function produceDrivingRange(r) {
   return function(a, b) {
     let len = parseInt(a.substring(0, 2)) - parseInt(b.substring(0, 2))
+          console.log(len)
     if (len > r) {
       return `${r - len} blocks out of range`
     } else {
       return `within range by ${len - r}`
-      console.log(len)
     }
   }
 }
