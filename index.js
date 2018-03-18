cc19a5c3cf58f42c2796534286648fd407cd4fdf
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 15:43:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function produceDrivingRange(r) {
+  return function(b, a) {
+    let len = parseInt(a.substring(0, 2)) - parseInt(b.substring(0, 2))
+          console.log(len)
+    if (len > r) {
+      return `${len - r} blocks out of range`
+    } else {
+      return `within range by ${r - len}`
+    }
+  }
+}
+
+function produceTipCalculator(p) {
+  return function(v) {
+    return v * p;
+  }
+}
