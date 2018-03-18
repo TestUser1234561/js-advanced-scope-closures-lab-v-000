95f336ca5d12ff9f2f9886bac7f9572ea14f0dbc
Morgan Fudge <mfudge92@gmail.com>
2018-03-18 15:42:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function produceDrivingRange(r) {
-  return function(a, b) {
+  return function(b, a) {
     let len = parseInt(a.substring(0, 2)) - parseInt(b.substring(0, 2))
           console.log(len)
     if (len > r) {
